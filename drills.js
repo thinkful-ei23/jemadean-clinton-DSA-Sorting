0e1e8621962f97d655bc201e452670dde49a507b
Clinton Owen <clint@clintonowen.com>
2018-10-11 15:55:59 -0500

    Complete shuffle in-place

--- drills.js
+++ drills.js
@@ -117,7 +117,8 @@
   
   // Bucket sort
   // Write an O(n) algorithm to sort an array of integers, where you know in advance what the lowest and highest values are.
-  function bSort(arr, low, hi) {
+  // Implemented technique described here: https://efficientcodeblog.wordpress.com/2017/12/18/bucket-sort-an-on-sorting-algorithm/
+  function bSort(arr) {
     let allBuckets = [];
     for (let i=0; i<arr.length; i++) {
       count++;
@@ -144,7 +145,20 @@
   
   // Sort in place
   // Write an algorithm to shuffle an array into a random order in-place (i.e. without creating a new array).
+  function shuffle(arr) {
+    for (let i=0; i<arr.length; i++) {
+      let j = Math.floor(Math.random() * arr.length);
+      let k = Math.floor(Math.random() * arr.length);
+      swap(arr, j, k);
+    }
+    return arr;
+  }
+  const sorted1 = [1,2,3,4,5,6,7,8,9];
+  const sorted2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50];
+  console.log('shuffle(sorted1) =', shuffle(sorted1));
+  console.log('shuffle(sorted2) =', shuffle(sorted2));
   
   // Sorting books
   // Imagine that I gave you twenty books to sort in alphabetical order. How would you go about it? Can you express this as an algorithm?
+  
 })();
